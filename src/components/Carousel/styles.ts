99949a29121a831641433c631d7c1a9c2339ddd2
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-22 19:34:20 -0300

    style: scratching carousel :tada:

--- src/components/Carousel/styles.ts
+++ src/components/Carousel/styles.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 10rem;
+  max-width: 60rem;
   width: 100%;
+  margin-top: 2rem;
+
+  background: red;
 `;
