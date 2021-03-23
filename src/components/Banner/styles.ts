6cf35dc28ade961b70f1015cd2d7ec61ad88cf68
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-23 10:59:41 -0300

    feat: banner new image and shadows :memo:

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
-import Banner from '../../assets/banner.png';
+import Banner from '../../assets/teste.png';
 
 export const Container = styled.img.attrs({
   src: Banner,
 })`
   z-index: -1;
+
+  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.5);
 `;
